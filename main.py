c57a24932a467bf39e62280669e52738b5698ebc
Juan Adad <juanadadbatero181@gmail.com>
2026-02-11 20:38:44 -0300

    chore: add runtime endpoint

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def prueba():
     return "Hola mundo"
+
+
+@app.get("/runtime")
+def runtime():
+    ## memory usage
+    ## CPU usage
+    ## Uptime
+    ## Execution Details
+    ## Pratform/Env Details
