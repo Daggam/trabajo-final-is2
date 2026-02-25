ec05afaaa075a71451351d503f44a9472bc87fb6
Benjamin Villa <daggamarg@gmail.com>
2026-02-25 15:10:03 -0300

    feat: Configurar app del proyecto.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Configuración",
+    description="API REST para consulta de versión, configuración y estado del sistema",
+    version="1.0.0"
+)
 
 @app.get("/")
 def prueba():
