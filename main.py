da8162664d5d54a48c42a7dd30ec5d27433eef2d
Victoria <gimenezmariavictoria95@gmail.com>
2026-02-26 22:45:37 -0300

    feat: Agregar endpoint /build

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import platform
+from settings import (
+    BUILD_TIME,
+    BASE_IMAGE
+)
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Configuración",
+    description="API REST para consulta de versión, configuración y estado del sistema",
+    version="1.0.0"
+)
 
-@app.get("/")
-def prueba():
-    return "Hola mundo"
+@app.get("/build")
+def build():
+    return {
+        "build_time": BUILD_TIME,
+        "base_image": BASE_IMAGE
+    }
+
